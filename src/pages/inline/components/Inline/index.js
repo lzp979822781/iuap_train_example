2a8417ba012ca3d2fc26d7d18732f4c87710f5b9
Kvkens <kvkens@qq.com>
2018-12-20 16:34:32 +0800

    行编辑初始化节点

--- src/pages/inline/components/Inline/index.js
+++ src/pages/inline/components/Inline/index.js
@@ -29,48 +29,14 @@ class Inline extends Component {
     componentDidMount() {
 
     }
-    //定义Grid的Column
-    column = [
-        {
-            title: "员工编号",
-            dataIndex: "code",
-            key: "code",
-            width: 150,
-            render: (text, record, index) => {
-                return <div>自定义数据列 {index}</div>
-            }
-        },
-        {
-            title: "员工姓名",
-            dataIndex: "name",
-            key: "name",
-            width: 120
-        },
-        {
-            title: "员工性别",
-            dataIndex: "sexEnumValue",
-            key: "sexEnumValue",
-            width: 120
-        },
-        {
-            title: "所属部门",
-            dataIndex: "deptName",
-            key: "deptName",
-            width: 120
-        },
-        {
-            title: "职级",
-            dataIndex: "levelName",
-            key: "levelName",
-            width: 120
-        }
-    ];
+
+
 
     render() {
 
         return (
             <div>
-                标准节点
+                行内编辑节点
             </div>
         )
     }
